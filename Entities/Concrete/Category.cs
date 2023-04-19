5b857c82f94da7dc3ac9afdd2e7edcbb49400d6c
tarik.onal <tarik.onal@csgb.gov.tr>
2023-04-19 09:22:43 +0300

    Add project files.

---
+++ Entities/Concrete/Category.cs
@@ -0,0 +1,12 @@
+﻿using Entities.Abstract;
+
+namespace Entities.Concrete
+{
+    //çıplak class kalmasın
+    public class Category : IEntity
+    {
+        
+        public int CategoryId { get; set; }
+        public string CategoryName { get; set; }
+    }
+}
---
+++ Entities/Concrete/Product.cs
@@ -0,0 +1,16 @@
+﻿using Entities.Abstract;
+
+namespace Entities.Concrete
+{
+    public class Product : IEntity
+    {
+        public int ProductId { get; set; }
+        public int CategoryId { get; set; }
+
+        public string ProductName { get; set; }
+
+        public int UnitsInStock { get; set; }
+
+        public decimal UnitPrice { get; set; }
+    }
+}

1994943ce616c064d847f7f62dc52cb4a1bdccb0
tarik.onal <tarik.onal@csgb.gov.tr>
2023-05-12 10:48:50 +0300

    Order Tablosu eklendi tüm katmanlara örnek olarak

--- DataAccess/Abstract/IOrderDal.cs
+++ DataAccess/Abstract/IOrderDal.cs
@@ -3,8 +3,7 @@ using Entities.Concrete;
 
 namespace DataAccess.Abstract
 {
-    public interface IProductDAL : IEntityRepository<Product>
+    public interface IOrderDal :IEntityRepository<Order>
     {
-    
     }
 }

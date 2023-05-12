16dbef6298f5f0681fee7cb1e48bca790cf21cdf
tarik.onal <tarik.onal@csgb.gov.tr>
2023-05-12 11:14:31 +0300

    Business sınıfları eklendi (Category)

--- ConsoleUI/Program.cs
+++ ConsoleUI/Program.cs
@@ -9,11 +9,26 @@ namespace ConsoleUI
     {
         static void Main(string[] args)
         {
+            //ProductTest();
+            CategoryTest();
+        }
+
+        private static void CategoryTest()
+        {
+            CategoryManager categoryManager = new CategoryManager(new EFCategoryDal());
+            foreach (var item in categoryManager.GetAll())
+            {
+                Console.WriteLine(item.CategoryId + " --> " + item.CategoryName);
+            }
+        }
+
+        private static void ProductTest()
+        {
             ProductManager productManager = new ProductManager(new EFProductDal());
 
-            foreach (var item in productManager.GetByUnitPrice(50,100))
+            foreach (var item in productManager.GetByUnitPrice(50, 100))
             {
-                Console.WriteLine(item.ProductName+ " --> " +item.UnitPrice);
+                Console.WriteLine(item.ProductName + " --> " + item.UnitPrice);
             }
         }
     }
